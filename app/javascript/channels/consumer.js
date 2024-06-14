e15ecb5a276a2e8cd042d2a68410ee8394ed15be
Caius Durling <dev@caius.name>
2024-06-14 09:57:53 +0100

    Swizzle ActionCable internal disconnect handling in dev/test
    This is triggered by calling `ActionCable.server.remote_connections.where(foo: true).disconnect` on the server, which is useful in development & tests to ensure logic works after reconnection.
    
    That is slow because it calls `Connection.close()` which closes the websocket and relies on `ConnectionMonitor` polling to fire once two heartbeats have been missed after ~6-8 seconds. Instead we want to call `Connection.reopen()` which closes the websocket, waits 500ms and opens it again. Same behaviour from the application's point of view, but much quicker from the human's point of view.

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,6 +1,27 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
 
-import { createConsumer } from "@rails/actioncable";
+import { createConsumer, Connection, logger } from "@rails/actioncable";
+
+const metaRailsEnv = document.querySelector('meta[name="rails_env"]');
+if (metaRailsEnv) {
+  // We're running in development or test, we want quicker (re)connections from ActionCable
+  console.log("Swizzling ActionCable Connection for faster reconnections in dev/test. See consumer.js for more info.");
+  const originalConnectionMessageHandler = Connection.prototype.events.message;
+  Connection.prototype.events.message = function (event) {
+    if (!this.isProtocolSupported()) {
+      return;
+    }
+    const { reason, reconnect, type } = JSON.parse(event.data);
+    if (type == "disconnect" && reconnect === true) {
+      logger.log(`Disconnect intercepted for reconnection. Reason: ${reason}`);
+      // This fires after 500ms, rather than waiting for two heartbeats to miss and ConnectionMonitor to
+      // reconnect a stale connection after 6+ seconds.
+      return this.reopen();
+    } else {
+      return originalConnectionMessageHandler.call(this, event);
+    }
+  };
+}
 
 export default createConsumer();
